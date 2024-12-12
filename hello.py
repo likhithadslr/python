1dba72e9ef3855edddd4798cf96c37bd7d63c563
likhithadslr <sreelikhithadurgempoodi@gmail.com>
2024-12-12 15:47:15 +0530

    python basics

---
+++ hello.py
@@ -0,0 +1,18 @@
+# print("Hellow world")
+# print(10+45-30/9+79*9)
+
+res=40*7
+# print(res)
+
+# to know the datatype
+# print(type(res))
+# print("Memory addr:",id(res)) # memory addr
+ 
+# print("result:",res)
+
+# a= 15.5
+# b= 50.2
+# res =a+b
+# print("result:", res , "type:",type(res))
+# print(type(res))
+
