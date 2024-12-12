c6945bb0bf70828e5ab8c6cf2e1409543c1b6ca4
likhithadslr <sreelikhithadurgempoodi@gmail.com>
2024-12-12 17:30:07 +0530

    python basics

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-# print("Hellow world")
+# print("Hello world")
 # print(10+45-30/9+79*9)
 
-res=40*7
+# res=40*7
 # print(res)
 
 # to know the datatype
@@ -43,4 +43,20 @@ res=40*7
 # print(txt) #prints whatever the letter we have at that index number
 
 # print(len(txt))
+
+# a=True
+# b=False
+# print(a and b) #false
+# print(a or b) #true
+
+# a=2
+# b=4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b) #modulo div -returns remainder
+# print(a**b) #exponentiation
+# print(a//b) #integer division
+
 
