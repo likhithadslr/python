9e3d6881eda5b78fe89d0891e8c78c67f945622d
likhithadslr <sreelikhithadurgempoodi@gmail.com>
2024-12-17 12:57:45 +0530

    python function basics

--- functions.py
+++ functions.py
@@ -14,11 +14,37 @@
 # ans=my_func(b=10,a=39)
 # print(ans+30)
 
-def my_func(a,b):
-    res=a+b
-    return res
+# def my_func(a,b):
+#     res=a+b
+#     return res
+# ans=my_func(b=10,a=39)
+# print(ans)
+# print(ans+30)
+# print(type(ans))
 
-ans=my_func(b=10,a=39)
-print(ans)
-print(ans+30)
+# name="Alice"
+# age=23
+# message="My name is " + name + " and I am " + str(age) + " years old."
+# print(message)
+
+# with f'string
+# name="Alice"
+# age=23
+# message=f"My name is {name} and I am {age} years old."
+# print(message)
+
+# def my_func(a,b,c=15):
+#     res = f"{a + c}"
+#     print(b)
+#     return res
+# ans = my_func(a=12,b={"c":123 , "a":320})
+# print(type(ans))
+
+# if u specify ** it is said as keyword arguments which is kwargs
+# def my_func(a,c=15,**b):
+#     res = f"{a + c}"
+#     print(type(b)) #dict
+#     return res
+# ans = my_func(a=12,b={"c":123 , "a":320})
+# print(type(ans))
 
