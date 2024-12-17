fc2462cd99e6a3c0eff3bdf78d266b661f9c07ad
likhithadslr <sreelikhithadurgempoodi@gmail.com>
2024-12-17 12:46:20 +0530

    python function basics

---
+++ functions.py
@@ -0,0 +1,24 @@
+# def my_func():
+#     print("Hello world")
+
+# my_func()
+
+# def my_func(a,b):
+#     res=a+b
+#     print(res)
+# my_func(10,39)
+
+# def my_func(a,b):
+#     res=a+b
+#     return res
+# ans=my_func(b=10,a=39)
+# print(ans+30)
+
+def my_func(a,b):
+    res=a+b
+    return res
+
+ans=my_func(b=10,a=39)
+print(ans)
+print(ans+30)
+
